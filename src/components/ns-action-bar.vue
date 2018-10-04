c1b794275fff9af7c12f0baf00460ca245c084ba
TJ VanToll <tj.vantoll@gmail.com>
2018-10-04 15:54:26 -0400

    Styling the ActionBar a bit

--- src/components/ns-action-bar.vue
+++ src/components/ns-action-bar.vue
@@ -26,3 +26,25 @@
         }
     }
 </script>
+
+<style>
+    :host div {
+        display: flex;
+        background-color: #efefef;
+        position: absolute;
+        width: 100%;
+        height: 3em;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: center;
+        align-content: center;
+        box-shadow: 0 0 5px rgba(0, 0, 0, .4);
+        text-align: center;
+        z-index: 1;
+    }
+    :host span {
+        display: inline-block;
+        flex-grow: 1;
+        font-size: 1.5em;
+    }
+</style>
