c116696793185a72cddde55fa0f6d8f5eb908935
Kamen Bundev <bundyo@gmail.com>
2018-10-09 16:08:15 +0300

    Add vueapp and ngapp apps. Get the vue one running.

---
+++ ngapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  entryComponents: [TestComponent]
+})
+export class AppModule { }
