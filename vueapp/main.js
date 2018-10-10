5e3441f321fc776f5f2976bf025be68714a64802
Kamen Bundev <bundyo@gmail.com>
2018-10-10 16:00:28 +0300

    Fix vue production build

--- vueapp/main.js
+++ vueapp/main.js
@@ -1,5 +1,8 @@
 import Vue from './nativescript-vue';
 import App from './components/App';
+
+import "app.scss";
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = false;
 
